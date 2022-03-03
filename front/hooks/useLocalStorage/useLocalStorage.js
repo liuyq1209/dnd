2f6bedf208450a25c4b1991ab3694a802a7e7d07
lyq <liuyq13159724332@163.com>
2022-03-03 14:58:08 +0800

    test

--- front/hooks/useLocalStorage/useLocalStorage.js
+++ front/hooks/useLocalStorage/useLocalStorage.js
@@ -1,35 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from "react"
 
 const useLocalStorage = (key, initialValue) => {
-	const isObject = () => {
-		return Object.prototype.toString.call(initialValue) === '[object Object]'
-	}
+  const isObject = () => {
+    return Object.prototype.toString.call(initialValue) === "[object Object]"
+  }
 
-	const [state, setState] = useState(() => {
-		try {
-			const item = window.localStorage.getItem(key)
-			console.log(item)
-			return isObject() && item ? JSON.parse(item) : item || initialValue
-		} catch (error) {
-			console.log(error)
-			return initialValue
-		}
-	})
+  const [state, setState] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      console.log(item)
+      return isObject() && item ? JSON.parse(item) : item || initialValue
+    } catch (error) {
+      return initialValue
+    }
+  })
 
-	const setLocalStorage = (newState) => {
-		try {
-			const newStateValue =
-				typeof newState === 'function' ? newState(state) : newState
-			setState(newStateValue)
-			window.localStorage.setItem(
-				key,
-				isObject() ? JSON.stringify(newStateValue) : newStateValue
-			)
-		} catch (error) {
-			console.log(`store ${key}'s new value failed, error info:`, error)
-		}
-	}
-	return [state, setLocalStorage]
+  const setLocalStorage = newState => {
+    try {
+      const newStateValue =
+        typeof newState === "function" ? newState(state) : newState
+      setState(newStateValue)
+      window.localStorage.setItem(
+        key,
+        isObject() ? JSON.stringify(newStateValue) : newStateValue
+      )
+    } catch (error) {
+      console.log(`store ${key}'s new value failed, error info:`, error)
+    }
+  }
+  return [state, setLocalStorage]
 }
 
 export default useLocalStorage
